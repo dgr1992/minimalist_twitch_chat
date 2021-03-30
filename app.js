a3f84d97f42b0bbd29c464fd118a901b72806b5c
dgr1992 <daniel@d7gr.at>
2021-03-30 11:04:14 +0200

    clean up and events

--- app.js
+++ app.js
@@ -27,12 +27,90 @@ function getRandomColor() {
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
+  if (color == '#000000'){
+    color = getRandomColor();
+  }
   return color;
 }
 
 function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+}
+
+function updateMessageBuffer(){
+  // Count the messages and start deleting old ones after threshold is reached
+  msgCounter += 1;
+  if (msgCounter >= 30) {
+    msgsElement.removeChild(msgsElement.lastChild);
+    countElement.textContent = msgCounter + " | 30";
+  }
+  else {
+    countElement.textContent = msgCounter + " | " + msgCounter;
+  }
+}
+
+function getUserColour(username, colour){
+  // user colour
+  if (colour == null) {
+    colour = dictUserColour[username];
+    if ( colour == null) {
+      colour = getRandomColor();
+      dictUserColour[username] = colour;
+    }
+  }
+
+  return colour;
+}
+
+function createTimeContainer(){
+  let currentDate = new Date();
+  let time = (currentDate.getHours()<10?'0':'') + currentDate.getHours() + ":" + (currentDate.getMinutes()<10?'0':'') + currentDate.getMinutes();
+  let containerTime = document.createElement("span");
+  let nodeTime = document.createTextNode (time);
+  containerTime.appendChild(nodeTime);
+
+  return containerTime;
+}
+
+function createUserContainer(username, displayName, color){
+  let containerUser = document.createElement("span");
+  let nodeUserDisplayName = document.createTextNode (displayName);
+  containerUser.appendChild(nodeUserDisplayName);
+  containerUser.style.color = getUserColour(username, color);
+
+  return containerUser;
+}
+
+function createEventTypeContainer(username, displayName, color){
+  let containerUser = document.createElement("span");
+  let nodeUserDisplayName = document.createTextNode (displayName);
+  containerUser.appendChild(nodeUserDisplayName);
+  containerUser.style.color = getUserColour(username, color);
+
+  return containerUser;
+}
+
+function createMessageContainer(message){
+  let containerMsg = document.createElement("span");
+  let nodeMsg = document.createTextNode (message);
+  containerMsg.appendChild(nodeMsg);
+
+  return containerMsg;
+}
+
+function createMessageElement(containerTime, containerUser, containerMsg){
+  // Create element to display - Format: "[Time] [userDisplayName]: [message]"
+  var container = document.createElement("div");
+  container.appendChild(containerTime);
+  let space = document.createTextNode(" ");
+  container.appendChild(space);
+  container.appendChild(containerUser);
+  let seperator = document.createTextNode(": ");
+  container.appendChild(seperator);
+  container.appendChild(containerMsg);
+
+  return container;
 }
 
 client.on('message', (wat, tags, message, self) => {
@@ -44,52 +122,39 @@ client.on('message', (wat, tags, message, self) => {
   if (username.includes("bot")) return;
 
   // Time
-  let currentDate = new Date();
-  let time = (currentDate.getHours()<10?'0':'') + currentDate.getHours() + ":" + (currentDate.getMinutes()<10?'0':'') + currentDate.getMinutes();
-  let containerTime = document.createElement("span");
-  let nodeTime = document.createTextNode (time);
-  containerTime.appendChild(nodeTime);
+  let containerTime = createTimeContainer();
 
   // Username
-  let containerUser = document.createElement("span");
-  let nodeUserDisplayName = document.createTextNode (displayName);
-  containerUser.appendChild(nodeUserDisplayName);
-  // user colour
-  if (color != null) {
-    containerUser.style.color = color;
-  }
-  else {
-    var userColour = dictUserColour[username];
-    if ( userColour == null) {
-      userColour = getRandomColor();
-      dictUserColour[username] = userColour;
-    }
-    containerUser.style.color = userColour;
-  }
+  let containerUser = createUserContainer(username, displayName, color);
 
   // Message
-  let containerMsg = document.createElement("span");
-  let nodeMsg = document.createTextNode (message);
-  containerMsg.appendChild(nodeMsg);
+  let containerMsg = createMessageContainer(message);
 
   // Create element to display
-  var container = document.createElement("div");
-  container.appendChild(containerTime);
-  let space = document.createTextNode(" ");
-  container.appendChild(space);
-  container.appendChild(containerUser);
-  let seperator = document.createTextNode(": ");
-  container.appendChild(seperator);
-  container.appendChild(containerMsg);
+  var container = createMessageElement(containerTime, containerUser, containerMsg);
+  
   msgsElement.prepend(container);
   
-  // Count the messages and start deleting old ones after threshold is reached
-  msgCounter += 1;
-  if (msgCounter >= 30) {
-    msgsElement.removeChild(msgsElement.lastChild);
-    countElement.textContent = msgCounter + " | 30";
-  }
-  else {
-    countElement.textContent = msgCounter + " | " + msgCounter;
-  }
+  updateMessageBuffer();
+});
+
+function createEventContainer(strEventType, strMessage){
+
+}
+
+client.on("subscription", (channel, username, method, message, userstate) => {
+  console.log("sub");
+});
+
+client.on("cheer", (channel, userstate, message) => {
+  const { username } = userstate;
+  const { color } = userstate;
+  const { 'display-name': displayName } = userstate;
+  const { bits } = userstate;
+  message = message.replace("cheer"+bits, "");
+  console.log(username + " ; " + color + " ; " + displayName + " ; " + bits + " ; " + message);
+});
+
+client.on("raided", (channel, username, viewers) => {
+  console.log("raided");
 });
